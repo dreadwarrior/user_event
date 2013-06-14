4a883001e927c9dbcf35f4cfe13ed8727fde2eee
Thomas Juhnke <tommy@van-tomas.de>
2013-06-14 15:20:59 +0200

    [TASK] removing copyright header

--- pi1/class.user_events_pi1.php
+++ pi1/class.user_events_pi1.php
@@ -1,40 +1,10 @@
 <?php
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2012 Oliver Thiele <mailYYYY@oliver-thiele.de>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 require_once(t3lib_extMgm::extPath('user_events') . '/Classes/Model/EventRepository.php');
 
 require_once(t3lib_extMgm::extPath('user_events') . '/Classes/View/StackedViewManager.php');
 require_once(t3lib_extMgm::extPath('user_events') . '/Classes/View/EventListView.php');
 require_once(t3lib_extMgm::extPath('user_events') . '/Classes/View/EventDetailView.php');
 
-/**
- * Plugin 'Event calendar' for the 'user_events' extension.
- *
- * @author	Oliver Thiele <mailYYYY@oliver-thiele.de>
- * @package	TYPO3
- * @subpackage	user_events
- */
 class user_events_pi1 extends tslib_pibase {
 
 	/**
